88f005efd27615cf2573dbfe015fdbe535962107
Monica Marziano <mmarziano1@gmail.com>
2018-05-20 23:40:57 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+ 
+attr_accessor :anagram 
+
+    def initialize(word)
+      @anagram = word
+    end
+    
+    def match(array)
+      sorted = @anagram.split(//).sort.join
+      array.each do |i| 
+        i.split(//).sort.join
+        return i if i == sorted
+      end
+    end
+end
