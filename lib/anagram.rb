07440eb817d41b218e3adf5d16da735aa6d229c0
Monica Marziano <mmarziano1@gmail.com>
2018-05-21 00:40:10 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Anagram
+ 
+attr_accessor :anagram 
+
+    def initialize(word)
+      @anagram = word
+    end
+    
+    def match(array)
+      list = []
+      sorted = @anagram.split(//).sort.join
+      array.each.select {|z| list << z if z.split(//).sort.join == sorted}
+      
+    end
+end
