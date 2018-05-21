28682b1327d86206cdef953a8ade0d266d3cf1f0
Monica Marziano <mmarziano1@gmail.com>
2018-05-21 00:13:30 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ attr_accessor :anagram
     def match(array)
       list = []
       sorted = @anagram.split(//).sort.join
-      #array.each_with_index {|i, v| i[v].split(//).sort.join}
+      array.each {|i| i.map(&:chars).flatten.sort.join}
       array.map(&:chars).flatten
       #list.select {|x| x == sorted}
     end
